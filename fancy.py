f89e6a5571105dc5e9fab4d1518451208d417226
AldrinL <634135016@qq.com>
2016-09-06 02:45:41 +0800

    Update fancy.py
    配置heroku版本

--- fancy.py
+++ fancy.py
@@ -80,8 +80,8 @@ def get_info(links):
 
 @app.before_first_request
 def update():
-    # client = MongoClient('mongodb://uPvyEVzH4jIWGhcn:pak0fPbeNsxdvH12D@10.10.72.139:27017/')
-    client = MongoClient()
+    client = MongoClient('mongodb://aldrin:yigepassword@ds019846.mlab.com:19846/fanci')
+    # client = MongoClient()
     db = client.test
     item_collection = db.item
     itemlist = get_info(get_links())
@@ -89,8 +89,8 @@ def update():
 
 @app.route('/')
 def index():
-    # client = MongoClient('mongodb://uPvyEVzH4jIWGhcn:pak0fPbeNsxdvH12D@10.10.72.139:27017/')
-    client = MongoClient()
+    client = MongoClient('mongodb://aldrin:yigepassword@ds019846.mlab.com:19846/fanci')
+    # client = MongoClient()
     db = client.test
     item_collection = db.item
     items = item_collection.find()
